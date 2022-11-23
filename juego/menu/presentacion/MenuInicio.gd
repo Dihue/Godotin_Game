52e806dd5bf505644b07e408a53b43cbb0413bce
Dihue <dihue21@gmail.com>
2022-11-23 11:31:09 -0300

    Update

---
+++ juego/menu/presentacion/MenuInicio.gd
@@ -0,0 +1,27 @@
+# MenuInicio.gd
+tool
+extends Control
+
+export(String, FILE, "*.tscn") var menu_ajustes = ""
+export(String, FILE, "*.tscn") var nivel_inicial = ""
+export(String, FILE, "*.tscn") var pantalla_carga = ""
+
+
+func _get_configuration_warning() -> String:
+	if menu_ajustes == "":
+		return "Chequear rutas"
+	
+	return ""
+
+
+func _on_BotonSalir_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_BotonOpciones_pressed() -> void:
+	get_tree().change_scene(menu_ajustes)
+
+
+func _on_BotonNuevo_pressed() -> void:
+	DatosJuego.nivel_actual = nivel_inicial
+	get_tree().change_scene(pantalla_carga)
--- juego/niveles/Nivel.gd
+++ juego/niveles/Nivel.gd
@@ -10,9 +10,8 @@ export(String, FILE, "*.tscn") var proximo_nivel = ""
 
 ## Metodos
 func _ready() -> void:
-	DatosJuego.nivel_actual = get_tree().current_scene.filename
-	DatosJuego.num_nivel_actual = numero_nivel
-	DatosJuego.nivel_proximo = proximo_nivel
+	yield(get_tree().create_timer(4.0),"timeout")
+	actualizar_datos()
 
 
 func _get_configuration_warning() -> String:
@@ -20,3 +19,9 @@ func _get_configuration_warning() -> String:
 		return "Chequear valores del nivel"
 	
 	return ""
+
+
+func actualizar_datos() -> void:
+	DatosJuego.nivel_actual = get_tree().current_scene.filename
+	DatosJuego.num_nivel_actual = numero_nivel
+	DatosJuego.nivel_proximo = proximo_nivel
